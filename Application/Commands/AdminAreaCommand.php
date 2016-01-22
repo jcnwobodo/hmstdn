58129bff81b67d2347a46b74bf7e4810363bc153
J. C. Nwobodo <jc.nwobodo@gmail.com>
2016-01-22 05:39:53 +0100

    27. Implement add-patient feature in admin-area

--- Application/Commands/AdminAreaCommand.php
+++ Application/Commands/AdminAreaCommand.php
@@ -12,6 +12,7 @@ namespace Application\Commands;
 use Application\Models\Category;
 use Application\Models\Employee;
 use Application\Models\EmploymentData;
+use Application\Models\Patient;
 use Application\Models\PersonalInfo;
 use Application\Models\Post;
 use Application\Models\User;
@@ -965,5 +966,132 @@ class AdminAreaCommand extends SecureCommand
         $data['page-title'] = "Add Staff (".ucwords($type).")";
         $requestContext->setResponseData($data);
         $requestContext->setView('admin/add-user.php');
+    }
+
+    protected function AddPatient(RequestContext $requestContext)
+    {
+        $data = array();
+
+        if($requestContext->fieldIsSet("add"))
+        {
+            $data['status'] = false;
+            $fields = $requestContext->getAllFields();
+
+            $card_number = $fields['card-number'];
+            $blood_group = $fields['blood-group'];
+            $genotype = $fields['genotype'];
+            $first_name = $fields['first-name'];
+            $last_name = $fields['last-name'];
+            $other_names = $fields['other-names'];
+            $gender = $fields['gender'];
+            $dob = $fields['date-of-birth'];
+            $nationality = $fields['nationality'];
+            $state_of_origin = $fields['state-of-origin'];
+            $lga_of_origin = $fields['lga-of-origin'];
+            $res_country = $fields['residence-country'];
+            $res_state = $fields['residence-state'];
+            $res_city = $fields['residence-city'];
+            $res_street = $fields['residence-street'];
+            $contact_email = $fields['contact-email'];
+            $contact_phone = $fields['contact-phone'];
+            $passport = !empty($_FILES['passport-photo']) ? $requestContext->getFile('passport-photo') : null;
+
+            $date_is_correct = checkdate($dob['month'], $dob['day'], $dob['year']);
+            $card_number_is_unique = is_null(Patient::getMapper('Patient')->findByCardNumber($card_number));
+            /*Ensure that mandatory data is supplied, then create a report object*/
+            if(
+                is_numeric($card_number) and strlen($card_number)==6 and $card_number_is_unique
+                and strlen($blood_group)
+                and strlen($genotype)
+                and strlen($first_name)
+                and strlen($last_name)
+                and in_array(strtolower($gender),PersonalInfo::$gender_enum)
+                and $date_is_correct
+                and strlen($nationality)
+                and strlen($state_of_origin)
+                and strlen($lga_of_origin)
+                and strlen($res_country)
+                and strlen($res_state)
+                and strlen($res_city)
+                and strlen($res_street)
+                and strlen($contact_email)
+                and (strlen($contact_phone)==11)
+                and !is_null($passport)
+            )
+            {
+                $date_of_birth = new DateTime(mktime(0,0,0,$dob['month'],$dob['day'],$dob['year']));
+
+                //Handle photo upload
+                $photo_handled = false;
+                $uploader = new UploadHandler('passport-photo', uniqid('passport_'));
+                $uploader->setAllowedExtensions(array('jpg'));
+                $uploader->setUploadDirectory("Uploads/passports");
+                $uploader->setMaxUploadSize(0.2);
+                $uploader->doUpload();
+
+                if($uploader->getUploadStatus())
+                {
+                    $photo = new Upload();
+                    //$photo->setAuthor($profile);
+                    $photo->setUploadTime(new DateTime());
+                    $photo->setLocation($uploader->getUploadDirectory());
+                    $photo->setFileName($uploader->getOutputFileName().".".$uploader->getFileExtension());
+                    $photo->setFileSize($uploader->getFileSize());
+
+                    $photo_handled = true;
+                }
+                else
+                {
+                    $data['status'] = false;
+                    $requestContext->setFlashData("Error Uploading Photo - ".$uploader->getStatusMessage());
+                }
+
+                if($photo_handled)
+                {
+                    $patient = new Patient();
+                    $patient->setCardNumber($card_number);
+                    $patient->setBloodGroup($blood_group);
+                    $patient->setGenotype($genotype);
+                    $patient->setStatus($patient::STATUS_ACTIVE);
+                    $patient->mapper()->insert($patient);
+
+                    $profile = new PersonalInfo();
+                    $profile->setId('p'.$patient->getId());
+                    $profile->setProfilePhoto($photo);
+                    $profile->setFirstName($first_name);
+                    $profile->setLastName($last_name);
+                    $profile->setOtherNames($other_names);
+                    $profile->setGender($gender);
+                    $profile->setDateOfBirth($date_of_birth);
+                    $profile->setNationality($nationality);
+                    $profile->setStateOfOrigin($state_of_origin);
+                    $profile->setLga($lga_of_origin);
+                    $profile->setResidenceCountry($res_country);
+                    $profile->setResidenceState($res_state);
+                    $profile->setResidenceCity($res_city);
+                    $profile->setResidenceStreet($res_street);
+                    $profile->setEmail(strtolower($contact_email));
+                    $profile->setPhone($contact_phone);
+
+                    $patient->setPersonalInfo($profile);
+
+                    $requestContext->setFlashData("Patient profile has been created successfully.");
+                    $data['status'] = true;
+                }
+            }
+            else{
+                $data['status'] = false;
+                $requestContext->setFlashData("Please fill out all fields with valid data, then try again.");
+
+                //Try returning more helpful error messages
+                if(strlen($card_number) != 6 or !is_numeric($card_number)) $requestContext->setFlashData("Card-Number must be a 6-digit number");
+                if(!$date_is_correct) $requestContext->setFlashData("Please supply a valid date for date of birth");
+                if(!$card_number_is_unique) $requestContext->setFlashData("Card number must be unique");
+            }
+        }
+
+        $data['page-title'] = "Add Patient";
+        $requestContext->setResponseData($data);
+        $requestContext->setView('admin/add-patient.php');
     }
 }
