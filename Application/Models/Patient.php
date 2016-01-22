249b77718cb47a1f01e781f945f12f84245f7ff7
J. C. Nwobodo <jc.nwobodo@gmail.com>
2016-01-22 01:54:13 +0100

    23. Create Classes Patient and PatientMapper

--- Application/Models/Patient.php
+++ Application/Models/Patient.php
@@ -10,10 +10,70 @@
 namespace Application\Models;
 
 
-class Patient extends DomainObject
+use System\Models\I_StatefulObject;
+
+class Patient extends DomainObject implements I_StatefulObject
 {
+    private $card_number;
+    private $personal_info;
+    private $status;
+
     public function __construct($id=null)
     {
         parent::__construct($id);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCardNumber()
+    {
+        return $this->card_number;
+    }
+
+    /**
+     * @param mixed $card_number
+     * @return Patient
+     */
+    public function setCardNumber($card_number)
+    {
+        $this->card_number = $card_number;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPersonalInfo()
+    {
+        return $this->personal_info;
+    }
+
+    /**
+     * @param mixed $personal_info
+     * @return Patient
+     */
+    public function setPersonalInfo(PersonalInfo $personal_info)
+    {
+        $this->personal_info = $personal_info;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     * @return Patient
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+        return $this;
     }
 }
